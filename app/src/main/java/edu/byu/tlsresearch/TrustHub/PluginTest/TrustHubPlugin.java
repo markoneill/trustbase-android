3c1f114c9990baaca052143dab3227de724c4c5d
ben <ben.scott.simpson@gmail.com>
2015-08-20 12:38:33 -0600

    Started abstract plugin

---
+++ app/src/main/java/edu/byu/tlsresearch/TrustHub/PluginTest/TrustHubPlugin.java
@@ -0,0 +1,9 @@
+package edu.byu.tlsresearch.TrustHub.PluginTest;
+
+/**
+ * Created by ben on 8/20/15.
+ * Abstract class that implements the plugin API and facilitates communication between the plugin and TrustHub.
+ */
+public class TrustHubPlugin {
+    //TODO: Write class
+}
